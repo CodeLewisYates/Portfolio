f806608faa1b54bfc0cb7a5e1091a56463dc5d69
CodeLewisYates <lewis_yates@ymail.com>
2021-03-27 12:33:50 +0000

    New project added

--- src/Page/Projects/Projects.js
+++ src/Page/Projects/Projects.js
@@ -6,23 +6,51 @@ const Projects = (props) => {
       <h1>My Projects</h1>
 
       <div className="projectsgrid">
-        <a href="https://palmtreegetaways.herokuapp.com/getaways" target="_blank" rel="noreferrer"><div
-          className="projectimage palmtree"
-        ></div></a>
-        <a href="https://eclaire.netlify.app/" target="_blank" rel="noreferrer"><div
-          className="projectimage eclaire"
-        ></div></a>
-        <a href="https://lewisnatours.herokuapp.com/" target="_blank" rel="noreferrer"><div
-          className="projectimage natours"
-        ></div></a>
-        <a href="https://react-burger-2f085.web.app/" target="_blank" rel="noreferrer"><div
-          className="projectimage burger"
-        ></div></a>
-        <a href="https://lewisapp.netlify.app/" target="_blank" rel="noreferrer"><div
-          className="projectimage forkify"
-        ></div></a>
-        <a href="https://chat-app-ly.netlify.app/" target="_blank" rel="noreferrer"><div className="projectimage chat-app">
-        </div></a>
+        <a
+          href="https://palmtreegetaways.herokuapp.com/getaways"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="projectimage palmtree"></div>
+        </a>
+        <a href="https://eclaire.netlify.app/" target="_blank" rel="noreferrer">
+          <div className="projectimage eclaire"></div>
+        </a>
+        <a
+          href="https://lewisnatours.herokuapp.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="projectimage natours"></div>
+        </a>
+        <a
+          href="https://react-burger-2f085.web.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="projectimage burger"></div>
+        </a>
+        <a
+          href="https://lewisapp.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="projectimage forkify"></div>
+        </a>
+        <a
+          href="https://chat-app-ly.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="projectimage chat-app"></div>
+        </a>
+        <a
+          href="https://concordly.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="projectimage concord"></div>
+        </a>
       </div>
     </div>
   );
